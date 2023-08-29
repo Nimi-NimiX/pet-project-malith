a7aa7d6cff53e2f243f0b16cb1235e1bbe9018c6
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-29 18:26:21 +0530

    defined users model schema and created users table

---
+++ pet-server/models/usersModel.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../database/databaseConnection");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 20],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    mobileNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [10, 15],
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 72],
+      },
+    },
+  },
+  {
+    tableName: "users",
+  }
+);
+
+module.exports = User;
