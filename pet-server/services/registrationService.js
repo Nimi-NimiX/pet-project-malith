6403bf16c9ddaa03a1d3ecd6e82c5948bd29c858
menaka809 <malithjayasekara2255721@gmail.com>
2023-08-31 17:31:04 +0530

    implemented  registerUserAndCompany function

--- pet-server/services/registrationService.js
+++ pet-server/services/registrationService.js
@@ -5,9 +5,56 @@ const companyPaySlipInfoRepository = require('../repository/companyPaySlipInfoRe
 
 const registerUserAndCompany = async (userData, companyData, paySlipData) => {
   try {
+    // Validate forme data
+    if (
+      !userData.username ||
+      !userData.email ||
+      !userData.mobileNumber ||
+      !userData.password
+    ) {
+      throw new Error('Incomplete user data.');
+    }
+
+    if (
+      !companyData.companyName ||
+      !companyData.companyEmail ||
+      !companyData.contactNumber ||
+      !companyData.field ||
+      !companyData.addressLine1 ||
+      !companyData.addressLine2 ||
+      !companyData.city ||
+      !companyData.country
+    ) {
+      throw new Error('Incomplete company data.');
+    }
+
+    if (!paySlipData.currency || !paySlipData.payday) {
+      throw new Error('Incomplete pay slip data.');
+    }
+
     // Hash the password before storing
     const hashedPassword = await bcrypt.hash(userData.password, 10);
     userData.password = hashedPassword;
+
+    // Register user
+    const user = await userRepository.createUser(userData);
+
+    // Register company
+    companyData.userId = user.id; // Attach the user ID to the companyData
+    const company = await companyRepository.createCompany(companyData);
+
+    // Register companyPaySlipInfo
+    paySlipData.companyId = company.id; // Attach the company ID to the paySlipData
+    const paySlipInfo =
+      await companyPaySlipInfoRepository.createCompanyPaySlipInfo(paySlipData);
+
+    return {
+      success: true,
+      message: 'Registration successful!',
+      userData: user,
+      companyData: company,
+      companyPaySlipInfo: paySlipInfo,
+    };
   } catch (error) {
     throw new Error('Registration process failed.');
   }
