e1a93f514e6551bee766d33a76d2cdde06dadd89
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-04 13:08:50 +0530

    added login controller to the routes

--- pet-server/routes/authRoute.js
+++ pet-server/routes/authRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const registrationController = require('../controllers/registrationController');
+const loginController = require('../controllers/loginController');
 
 router.post('/register', registrationController.register);
+router.post('/login', loginController.login);
 
 module.exports = router;
