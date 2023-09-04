dc403dc6a6dc807a90c2064cf7f530a961e66b25
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-04 17:50:01 +0530

    created seperate service for the update user profile

--- pet-server/services/profileService.js
+++ pet-server/services/profileService.js
@@ -14,3 +14,28 @@ const getUserProfile = async (userId) => {
     throw new Error(`Failed to fetch user profile: ${error.message}`);
   }
 };
+
+const updateUserProfile = async (userId, updatedProfile) => {
+  try {
+    // Validate and sanitize input data
+    const { error } = validateUserProfileUpdate(updatedProfile);
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
+    const user = await usersRepository.getUserById(userId);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Update user profile
+    const result = await usersRepository.updateUser(user, updatedProfile);
+
+    return result;
+  } catch (error) {
+    throw new Error(`Profile update failed: ${error.message}`);
+  }
+};
+
+module.exports = { getUserProfile, updateUserProfile };
