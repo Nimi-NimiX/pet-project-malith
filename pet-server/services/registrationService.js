70a1fd2408b1776c475f76602f4f7325bf08a2a2
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-04 12:42:27 +0530

    craeted service for the login user

--- pet-server/services/registrationService.js
+++ pet-server/services/registrationService.js
@@ -6,8 +6,10 @@ const {
   validateUserData,
   validateCompanyData,
   validatePaySlipData,
-  validateAndThrow,
+  validateLoginData,
 } = require('../utils/validation');
+
+const secretKey = process.env.JWT_SECRET_KEY;
 
 const registerUserAndCompany = async (userData, companyData, paySlipData) => {
   try {
@@ -59,6 +61,37 @@ const registerUserAndCompany = async (userData, companyData, paySlipData) => {
   }
 };
 
+const loginUser = async (email, password) => {
+  try {
+    // Validate user input
+    const { error } = validateLoginData({ email, password });
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
+    // Retrieve the user from the database based on the provided email
+    const user = await userRepository.getUserByEmail(email);
+    if (!user) {
+      throw new Error(`User dosen't exsists`);
+    }
+
+    // Compare the provided password with the hashed password using bcrypt
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error('Invalid password');
+    }
+
+    // Generate a JWT token for authentication
+    const token = jwt.sign({ userId: user.id }, secretKey, {
+      expiresIn: '1h', // Token expires in 1 hour (adjust as needed)
+    });
+    return { success: true, message: 'Login successful', token };
+  } catch (error) {
+    throw new Error(`Login failed: ${error.message}`);
+  }
+};
+
 module.exports = {
   registerUserAndCompany,
+  loginUser,
 };
