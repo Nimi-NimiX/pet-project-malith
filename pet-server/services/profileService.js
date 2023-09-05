89493f8ac3227c528868a11c870cd8b932be6ab8
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-05 19:00:28 +0530

    implemented controllers for get company and update company

--- pet-server/services/profileService.js
+++ pet-server/services/profileService.js
@@ -1,5 +1,6 @@
 const UserRrepository = require('../repository/userRepository');
 const Validation = require('../utils/validation');
+const CompanyRepository = require('../repository/companyRepository');
 
 const ProfileService = {
   getUserProfile: async (userId) => {
@@ -39,6 +40,36 @@ const ProfileService = {
       return result;
     } catch (error) {
       throw new Error(`Profile update failed: ${error.message}`);
+    }
+  },
+  getCompanyProfile: async (companyId) => {
+    try {
+      const company = await CompanyRepository.findById(companyId);
+      return company;
+    } catch (error) {
+      throw new Error(`Failed to fetch company profile: ${error.message}`);
+    }
+  },
+
+  updateCompanyProfile: async (companyId, updatedProfile) => {
+    try {
+      // Validate and sanitize input data (use CompanyValidation)
+      const { error } = Validation.validateCompanyProfileUpdate(updatedProfile);
+      if (error) {
+        throw new Error(error.details[0].message);
+      }
+
+      const company = await CompanyRepository.findById(companyId);
+
+      // Update company profile using the repository method
+      const result = await CompanyRepository.updateCompanyProfile(
+        company,
+        updatedProfile
+      );
+
+      return result;
+    } catch (error) {
+      throw new Error(`Company profile update failed: ${error.message}`);
     }
   },
 };
