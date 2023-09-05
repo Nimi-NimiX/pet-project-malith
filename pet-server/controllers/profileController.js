c573a3bc9884c2cebac7acb888326d64248204c0
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-05 13:43:27 +0530

    implemented service for get user profile

--- pet-server/controllers/profileController.js
+++ pet-server/controllers/profileController.js
@@ -1,1 +1,16 @@
+const ProfileService = require('../services/profileService');
 
+const ProfileController = {
+  getUserProfile: async (req, res, next) => {
+    try {
+      const userId = req.user.id; // Assuming you have authentication middleware that sets req.user
+      const user = await ProfileService.getUserProfile(userId);
+
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+module.exports = ProfileController;
