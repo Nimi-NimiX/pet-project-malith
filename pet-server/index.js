41beb686edb45cee19d96867d3eab42c10a4a62f
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-05 16:22:24 +0530

    initialize authenticate middleware

--- pet-server/index.js
+++ pet-server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const passport = require('passport');
 const authRoutes = require('./routes/authRoute');
+const profileRoutes = require('./routes/profileRoute');
+const userConfig = require('./configuration/userConfig');
 const logger = require('./utils/logger');
 const databaseConnect = require('./database/databaseConnection');
 const synchronizeDatabase = require('./database/synchronizeDatabase');
@@ -11,10 +14,13 @@ require('dotenv').config();
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 
 //routes
 //Use the auth routes for registration and login
 app.use('/auth', authRoutes);
+//Use the profile routes for user profile and company profile
+app.use('/api', profileRoutes);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`server has started on port ${process.env.SERVER_PORT}`);
