3581f338eaf55867ba17f34b4d3b5bb16f369846
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-05 12:51:14 +0530

    wrap all the profile services into constant

--- pet-server/services/profileService.js
+++ pet-server/services/profileService.js
@@ -1,41 +1,43 @@
 const usersRepository = require('../repository/usersRepository');
 const { validateUserProfileUpdate } = require('../utils/validation');
 
-const getUserProfile = async (userId) => {
-  try {
-    const user = await usersRepository.getUserById(userId);
+const ProfileService = {
+  getUserProfile: async (userId) => {
+    try {
+      const user = await usersRepository.getUserById(userId);
 
-    if (!user) {
-      throw new Error('User not found');
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      throw new Error(`Failed to fetch user profile: ${error.message}`);
     }
+  },
 
-    return user;
-  } catch (error) {
-    throw new Error(`Failed to fetch user profile: ${error.message}`);
-  }
+  updateUserProfile: async (userId, updatedProfile) => {
+    try {
+      // Validate and sanitize input data
+      const { error } = validateUserProfileUpdate(updatedProfile);
+      if (error) {
+        throw new Error(error.details[0].message);
+      }
+
+      const user = await usersRepository.getUserById(userId);
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      // Update user profile
+      const result = await usersRepository.updateUser(user, updatedProfile);
+
+      return result;
+    } catch (error) {
+      throw new Error(`Profile update failed: ${error.message}`);
+    }
+  },
 };
 
-const updateUserProfile = async (userId, updatedProfile) => {
-  try {
-    // Validate and sanitize input data
-    const { error } = validateUserProfileUpdate(updatedProfile);
-    if (error) {
-      throw new Error(error.details[0].message);
-    }
-
-    const user = await usersRepository.getUserById(userId);
-
-    if (!user) {
-      throw new Error('User not found');
-    }
-
-    // Update user profile
-    const result = await usersRepository.updateUser(user, updatedProfile);
-
-    return result;
-  } catch (error) {
-    throw new Error(`Profile update failed: ${error.message}`);
-  }
-};
-
-module.exports = { getUserProfile, updateUserProfile };
+module.exports = ProfileService;
