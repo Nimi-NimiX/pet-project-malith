446e83e64e2e68101301cf7c356c857a29a8c518
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-10 13:33:11 +0530

    implemented findByUserId , findByCompanyId and updatePaySlipDetails methods

--- pet-server/repository/companyPaySlipInfoRepository.js
+++ pet-server/repository/companyPaySlipInfoRepository.js
@@ -9,6 +9,35 @@ const CompanyPaySlipRrepository = {
       return error;
     }
   },
+  findByUserId: async (userId) => {
+    try {
+      const company = await Company.findOne({ where: { userId } });
+      return company;
+    } catch (error) {
+      throw error;
+    }
+  },
+  findByCompanyId: async (companyId) => {
+    try {
+      console.log(`pay slip companyid: ${companyId}`);
+      const companyPaySlip = await CompanyPaySlipInfo.findOne({
+        where: { companyId },
+      });
+      console.log(`pay slip ${companyPaySlip}`);
+      return companyPaySlip;
+    } catch (error) {
+      throw error;
+    }
+  },
+  updatePaySlipDetails: async (updatedPaySlipDetails) => {
+    try {
+      // Save the updated user data
+      await updatedPaySlipDetails.save();
+      return updatedPaySlipDetails;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
 
 module.exports = CompanyPaySlipRrepository;
