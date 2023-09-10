730791f764b3bc64b2bd1eb8b08256ba282673c1
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-10 13:38:17 +0530

    move business logic into profile service

--- pet-server/services/profileService.js
+++ pet-server/services/profileService.js
@@ -1,6 +1,7 @@
 const UserRrepository = require('../repository/userRepository');
 const Validation = require('../utils/validation');
 const CompanyRepository = require('../repository/companyRepository');
+const CompanyPaySlipRrepository = require('../repository/companyPaySlipInfoRepository');
 
 const ProfileService = {
   getUserProfile: async (userId) => {
@@ -30,42 +31,97 @@ const ProfileService = {
         throw new Error('User not found');
       }
 
-      // Update user profile
-      const result = await UserRrepository.updateUserProfile(
-        userId,
-        updatedProfile
-      );
+      // Update user-specific fields
+      if (updatedProfile.username) user.username = updatedProfile.username;
+      if (updatedProfile.email) user.email = updatedProfile.email;
+      if (updatedProfile.mobileNumber)
+        user.mobileNumber = updatedProfile.mobileNumber;
+
+      // Save the updated user profile using the repository
+      const result = await UserRrepository.updateUserProfile(user);
 
       return result;
     } catch (error) {
       throw new Error(`Profile update failed: ${error.message}`);
     }
   },
-  getCompanyProfile: async (companyId) => {
+  getCompanyProfile: async (userId) => {
     try {
-      const company = await CompanyRepository.findById(companyId);
+      const company = await CompanyRepository.findByUserId(userId);
       return company;
     } catch (error) {
       throw new Error(`Failed to fetch company profile: ${error.message}`);
     }
   },
-
-  updateCompanyProfile: async (companyId, updatedProfile) => {
+  getPaySlipProfile: async (companyId) => {
     try {
-      const { error } = Validation.validateCompanyProfileUpdate(updatedProfile);
+      const companyPaySlip = await CompanyPaySlipRrepository.findByCompanyId(
+        companyId
+      );
+
+      console.log(`company pay slip :${companyPaySlip}`);
+      return companyPaySlip;
+    } catch (error) {
+      throw new Error(`Failed to fetch company profile: ${error.message}`);
+    }
+  },
+
+  updateCompanyProfile: async (userId, updatedComProfile) => {
+    try {
+      const { error } =
+        Validation.validateCompanyProfileUpdate(updatedComProfile);
       if (error) {
         throw new Error(error.details[0].message);
       }
 
-      const company = await CompanyRepository.findById(companyId);
+      const company = await CompanyRepository.findByUserId(userId);
 
-      // Update company profile using the repository method
-      const result = await CompanyRepository.updateCompanyProfile(
-        company,
-        updatedProfile
+      if (!company) {
+        throw new Error('company  not found');
+      }
+
+      // Update company-specific fields
+      if (updatedComProfile.companyName)
+        company.companyName = updatedComProfile.companyName;
+      if (updatedComProfile.companyEmail)
+        company.companyEmail = updatedComProfile.companyEmail;
+      if (updatedComProfile.contactNumber)
+        company.contactNumber = updatedComProfile.contactNumber;
+      if (updatedComProfile.field) company.field = updatedComProfile.field;
+      if (updatedComProfile.addressLine1)
+        company.addressLine1 = updatedComProfile.addressLine1;
+      if (updatedComProfile.addressLine2)
+        company.addressLine2 = updatedComProfile.addressLine2;
+      if (updatedComProfile.mobileNumber)
+        company.mobileNumber = updatedComProfile.mobileNumber;
+      if (updatedComProfile.city) company.city = updatedComProfile.city;
+      if (updatedComProfile.country)
+        company.country = updatedComProfile.country;
+
+      const updatedCompany = await CompanyRepository.updateCompanyProfile(
+        company
       );
 
-      return result;
+      const companyPaySlip = await CompanyPaySlipRrepository.findByCompanyId(
+        company.id
+      );
+
+      if (!companyPaySlip) {
+        throw new Error('Pay slip details not found');
+      }
+
+      if (updatedComProfile.currency)
+        companyPaySlip.currency = updatedComProfile.currency;
+      if (updatedComProfile.payday)
+        companyPaySlip.payday = updatedComProfile.payday;
+
+      const updatedPaySlip =
+        await CompanyPaySlipRrepository.updatePaySlipDetails(companyPaySlip);
+
+      return {
+        company: updatedCompany,
+        paySlip: updatedPaySlip,
+      };
     } catch (error) {
       throw new Error(`Company profile update failed: ${error.message}`);
     }
