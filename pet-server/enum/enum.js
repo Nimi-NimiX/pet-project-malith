a9c1357964597687f08d4428dc86a16050723c77
menaka809 <malithjayasekara2255721@gmail.com>
2023-09-11 07:55:39 +0530

    created drop down list for company field and currency

---
+++ pet-server/enum/enum.js
@@ -0,0 +1,39 @@
+const CategoryTpes = {
+  companyFieldEnum: Object.freeze({
+    IT: 'IT sector',
+    Finance: 'Finance sector',
+    Healthcare: 'Healthcare sector',
+  }),
+
+  defaultCurrencyEnum: Object.freeze({
+    USD: 'US Dollar(USD) ',
+    EUR: 'Euro(EUR) ',
+    GBP: 'British Pound(GBP) ',
+    JPY: 'Japanese Yen(JPY) ',
+    AUD: 'Australian Dollar(AUD) ',
+    CAD: 'Canadian Dollar(CAD) ',
+    CHF: 'Swiss Franc(CHF) ',
+    CNY: 'Chinese Yuan(CNY) ',
+    INR: 'Indian Rupee(INR) ',
+    RUB: 'Russian Ruble(RUB) ',
+    BRL: 'Brazilian Real(BRL) ',
+    ZAR: 'South African Rand(ZAR) ',
+    NZD: 'New Zealand Dollar(NZD) ',
+    SGD: 'Singapore Dollar(SGD) ',
+    HKD: 'Hong Kong Dollar(HKD) ',
+    SEK: 'Swedish Krona(SEK) ',
+    NOK: 'Norwegian Krone(NOK) ',
+    DKK: 'Danish Krone(DKK) ',
+    KRW: 'South Korean Won(KRW) ',
+    AED: 'United Arab Emirates Dirham(AED) ',
+    SAR: 'Saudi Riyal(SAR) ',
+    MXN: 'Mexican Peso(MXN) ',
+    ARS: 'Argentine Peso(ARS) ',
+    CLP: 'Chilean Peso(CLP) ',
+    PLN: 'Polish Złoty(PLN) ',
+    TRY: 'Turkish Lira(TRY) ',
+    LKR: 'Sri Lankan Rupee(LKR) ',
+  }),
+};
+
+module.exports = CategoryTpes;
